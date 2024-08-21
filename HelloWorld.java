7e16c0989c3eae723d0694c8df0d2629f10a52ea
CHANG CHI MING <71378373+12687426807569@users.noreply.github.com>
2024-08-22 01:50:10 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,12 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("Hello world!");
+		System.out.print("Hello world!\n輸入年齡:");
+		java.util.Scanner s=new java.util.Scanner(System.in);
+		int age=s.nextInt();//不吃\n
+		System.out.print("年齡="+age+"\n輸入名字:");
+		s.nextLine();
+		String name=s.nextLine();//吃\n但不回傳
+		System.out.print("你好"+name);
+		System.out.print("---");
 	}
 }
